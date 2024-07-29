a224703fd4813569412f70ba216778aaf3496da0
Casperavr <casper@hhc.earth>
2024-07-29 15:07:54 +0200

    add new tree class instances if the array is not full, and move logs down if the bottom log is not at the bottom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // create instances
-const myLumberjack = new Player(); 
-const firstLog = new TreeLog(0); 
+const myLumberjack = new Player();
+// first log has no branch 
+const firstLog = new TreeLog(1); 
 
-//global variables
-
+// global variables
+let treeArray = [];
+treeArray.push(firstLog);
 
 /* functionality    -   -   -   -   -   -   -   -   -   -*/
 
@@ -13,29 +15,67 @@ document.addEventListener("keydown", (event) => {
     console.log(event.code)
     if(event.code === 'ArrowLeft'){
         myLumberjack.moveLeft();
+        treeArray[0].removeLog();
+        treeArray.shift();
     } else 
     
     if(event.code === 'KeyA'){
         myLumberjack.moveLeft();
+        treeArray[0].removeLog();
+        treeArray.shift();
     } else 
 
     if(event.code === 'Comma'){
         myLumberjack.moveLeft();
+        treeArray[0].removeLog();
+        treeArray.shift();
     } else 
     
     if(event.code === 'ArrowRight'){
         myLumberjack.moveRight();
-    }
+        treeArray[0].removeLog();
+        treeArray.shift();
+    } else
 
     if(event.code === 'KeyD'){
         myLumberjack.moveRight();
-    }
+        treeArray[0].removeLog();
+        treeArray.shift();
+    } else
 
     if(event.code === 'Period'){
         myLumberjack.moveRight();
+        treeArray[0].removeLog();
+        treeArray.shift();
     }
 });
 
 
 
-//log movement
+// log movement and spawning
+setInterval(() => {
+
+    // moving the logs down over the tree array
+    if(treeArray[0].positionY > 0){
+        for(let i = treeArray.length -1; i >= 0; i--){
+            treeArray[i].moveDown();
+            console.log("moved down");
+        }
+    }
+
+    
+    // adding new logs if the screen is not filled with logs
+    if(treeArray.length < 4){
+        treeArray.push(new TreeLog(Math.floor(Math.random() * 3)))
+        console.log("new log added")
+    }
+    
+}, 100);
+
+
+
+
+// breaking logs
+
+
+
--- src/tree.js
+++ src/tree.js
@@ -1,16 +1,19 @@
 class TreeLog{
     constructor(branchpos){
+        //log
         this.width = 8;
         this.height = 20;                      // as tall as player
         this.positionX = 33.5;                 // center of screen
         this.positionY = 60;
+
+        //branches
         this.branchPosition = branchpos;       // 0 is left, 1 is none, 2 is right
         this.branchWidth = 8;
         this.branchHeight = 2;
-        this.branchPosX = branchpos === 0 ? 25.5 : 41.5;                // 25.5 is left, 41.5 is right
-        this.branchPosY = this.positionY + 9;
-
+        this.branchPosX = branchpos === 0 ? 25.5 : 41.5;  // 25.5 is left, 41.5 is right
+        this.branchPosY = this.positionY + 2;
         this.createtreeElement();               
+
 
         // only create a branchelement if there is a branchposition
         if(this.branchPosition !== 1){         
@@ -46,6 +49,18 @@ class TreeLog{
 
     moveDown(){
         this.positionY -= 20;
+        this.branchPosY = this.positionY + 2;
         this.treeElement.style.bottom = `${this.positionY}vh`;
+
+        if(this.branchElement){
+            this.branchElement.style.bottom = `${this.branchPosY}vh`;
+        }
+    }
+
+    removeLog(){
+        this.treeElement.remove()
+        if(this.branchElement){
+            this.branchElement.remove()
+        }
     }
 }
